18f91657832298106dc59caa6fcb01d877dd81a9
PimonovDaniil <PimonovDaniil@users.noreply.github.com>
2020-03-08 03:53:21 +0700

    Ну вроде обучение сделал
    Но надо тестить и отлаживать и юзать кст ещё надо и тд

--- digNeyro.py
+++ digNeyro.py
@@ -1,3 +1,6 @@
+import json
+from sklearn.datasets import fetch_openml
+
 def w_sum(a, b):
     assert(len(a) == len(b))
     output = 0
@@ -19,17 +22,42 @@ def ele_mul(delta, input):
         output[i]= delta*input[i]
     return output
 
-weights = [[0.1, 0.2, -0.1],[0.1, 0.2, -0.1]]
-input= [8.5, 0.65, 1.2] #вход
-really = [2, 3] #выход
-alpha = 0.01
+#загружаем файл с весами
+# with open("weight.txt", "r") as write_file:
+#     weights = json.load(write_file) #10*784
 
-for k in range(len(really)):
-    for j in range(10):#цикл для обучения нейронов
-        pred = neural_network(input, weights[k])
-        delta = (pred - really[k])
-        weight_deltas = ele_mul(delta, input)
+#генерируем структуру
+weights=[]
+for i in range(10):
+    s=[]
+    for j in range(784):
+        s.append(0.5)
+    weights.append(s)
+print(weights)
 
-        for i in range(len(weights)):
-            weights[k][i] -= weight_deltas[i] * alpha
-    print(pred)
+#загружаем данные для обучения
+mnist = fetch_openml('mnist_784')
+
+alpha = 0.0000001 #чистая магия
+
+for j in range(10000):#входные даныные
+    print(j)
+    #print(mnist['target'][j])
+    input= mnist['data'][j] #вход 784
+    really = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] #выход 10 нейронов
+    really[int(mnist['target'][j])] = 5.0
+    for k in range(len(really)):#обучаем один набор нейросети (k - кол-во выходов)
+        error=10000#большое число
+        while(error>0.01):#Пока ошибка не станет приемлемой
+            pred = neural_network(input, weights[k])
+            delta = (pred - really[k])
+            weight_deltas = ele_mul(delta, input)
+            error = (pred - really[k])**2
+            # print("error " + str(error))
+            # print("pred: " + str(pred))
+            for i in range(len(weights[k])):
+                weights[k][i] -= weight_deltas[i] * alpha
+        #print("pred: " + str(pred))
+
+with open("weight.txt", "w") as write_file:
+    json.dump(weights, write_file)# сохраняем веса
